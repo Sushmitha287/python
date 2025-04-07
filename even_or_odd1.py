c45c01c983a50a53f1c74d5b69331a439922db51
Sushmitha287 <ravadagundisushmitha@gmail.com>
2025-04-07 22:59:05 +0530

    first commit

---
+++ even_or_odd1.py
@@ -0,0 +1,10 @@
+#2. Using Integer Division operator (//)
+
+number = int(input("Enter a number:"))
+if (number // 2) * 2 == number:
+	print(number, 'is even.')
+else:
+	print(number, 'is an odd')
+
+
+#print(f"{number} is an odd." )
---
+++ positive_negative_or_zero.py
@@ -0,0 +1,10 @@
+#1. Using if-elif-else Statements
+
+
+number = int(input("Enter a number:"))
+if number > 0:
+	print(number, 'is a positive number.')
+elif number < 0:
+	print(number, 'is a negative number.')
+else:
+	print('Zero')
